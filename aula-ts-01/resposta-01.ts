6b3d6478d6dec4375089182600c6b0eb5ceae0e4
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-09-12 18:04:49 -0300

    aula-ts-03

--- aula-ts-01/resposta-01.ts
+++ aula-ts-01/resposta-01.ts
@@ -1,6 +1,6 @@
-function somar(num1 : number, num2 : number) {
+function somar(num1: number, num2: number): number {
     return num1 + num2;
 }
 
-const resultado : number = somar(2, 3);
+const resultado: number = somar(2, 3);
 console.log(resultado);
--- aula-ts-02/src/resposta-02.ts
+++ aula-ts-02/src/resposta-02.ts
@@ -1,6 +1,6 @@
-function sum(num1: number, num2: number) {
+function sum(num1: number, num2: number): number {
     return num1 + num2;
   }
   
-  const result : number = sum(1, 2);
+  const result: number = sum(1, 2);
   console.log(result); // 3
